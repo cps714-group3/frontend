0dd7bb416f7e686d0d7ad3dff280918fd7f46df9
Nabeel Asim <nabeelasim0250@gmail.com>
2022-11-17 23:35:30 -0500

    Added user signed-in check

--- src/pages/ProjectSettings.tsx
+++ src/pages/ProjectSettings.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { useToast } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import { useSigninCheck, useUser } from "reactfire";
 import './ProjectSettings.css';
+
 
 export const ProjectSettings = () => {
 
     // Button redirecting to the project settings page will only be visible if there is an active project
     // i.e., a project with "In Progress" status
+
+    const {data: user} = useUser();
+    const navigate = useNavigate();
+    const toast = useToast();
+
+    const { status, data: signInCheckResult } = useSigninCheck();
+
+    React.useEffect(() => {
+        if (status === 'success') {
+            if (!signInCheckResult.signedIn) {
+                toast({
+                    title: 'Cannot Access Dashboard',
+                    description: 'User is not Authenticated',
+                    status: 'error',
+                    duration: 3500
+                })
+                navigate('/login');
+            } else {
+                console.log(user);
+            }
+
+        }
+    }, [signInCheckResult, status, user])
 
     type ProjectSettingsType = {
         projectName: string
