39cbe2bc92c987382070fe6e6b91679696485995
Monika T <81535019+Mon-0987@users.noreply.github.com>
2022-11-24 14:17:02 -0500

    sidebar & subheader compoennt added

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import { Box, Center, Heading, Text } from '@chakra-ui/react';
-import { useFirebaseApp, AuthProvider } from 'reactfire';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import './App.css';
-import { getAuth } from 'firebase/auth';
-import { Landing } from './pages/Landing';
-import { Login } from './pages/Login';
-import { Signup } from './pages/Signup';
-import { LandingNav } from './components/header/Header';
-import { Footer } from './components/footer/Footer';
-import { Reports } from './pages/Reports';
-import { WorkBoard } from './pages/Boardpage';
-import { Dashboard } from './pages/Dashboard';
-import { ProjectSettings } from './pages/ProjectSettings';
+import React from "react";
+import { Box, Center, Heading, Text } from "@chakra-ui/react";
+import { useFirebaseApp, AuthProvider } from "reactfire";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import "./App.css";
+import { getAuth } from "firebase/auth";
+import { Landing } from "./pages/Landing";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import { LandingNav } from "./components/header/Header";
+import { Footer } from "./components/footer/Footer";
+import { Reports } from "./pages/Reports";
+import { WorkBoard } from "./pages/Boardpage";
+import { Dashboard } from "./pages/Dashboard";
+import { ProjectSettings } from "./pages/ProjectSettings";
 
 function App() {
-    const app = useFirebaseApp();
-    const auth = getAuth(app);
+  const app = useFirebaseApp();
+  const auth = getAuth(app);
 
-    return (
-        <AuthProvider sdk={auth}>
-            <LandingNav />
-            <Routes>
-                <Route path='/' element={<Landing />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/signup' element={<Signup />} />
-                <Route path='/board' element={<WorkBoard />} />
-                <Route path='/reports' element={<Reports />} />
-                <Route path='/dashboard' element={<Dashboard />} />
-                <Route path='/project-settings' element={<ProjectSettings />} />
-            </Routes>
-            <Footer />
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider sdk={auth}>
+      <LandingNav />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/board" element={<WorkBoard />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/project-settings" element={<ProjectSettings />} />
+      </Routes>
+      <Footer />
+    </AuthProvider>
+  );
 }
 
 export default App;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,234 +1,269 @@
 import {
-    Box,
-    Button,
-    Center,
-    HStack,
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    ModalHeader,
-    ModalOverlay,
-    useDisclosure,
-    useToast,
-    VStack,
-} from '@chakra-ui/react';
-import { SingleDatepicker } from 'chakra-dayzed-datepicker';
-import { Form, Formik, FormikHelpers } from 'formik';
-import axios, { AxiosError } from 'axios';
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSigninCheck, useUser } from 'reactfire';
-import { date, mixed, object, string } from 'yup';
-import { DatePickerFormControl } from '../components/form/DatePickerFormControl';
-import { InputFormControl } from '../components/form/InputFormControl';
-import { usePageHeight } from '../helpers/hooks';
+  Box,
+  Button,
+  Center,
+  HStack,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
+import "./Dashboard.css";
+import { Form, Formik, FormikHelpers } from "formik";
+import axios, { AxiosError } from "axios";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSigninCheck, useUser } from "reactfire";
+import { date, mixed, object, string } from "yup";
+import { DatePickerFormControl } from "../components/form/DatePickerFormControl";
+import { InputFormControl } from "../components/form/InputFormControl";
+import { usePageHeight } from "../helpers/hooks";
+import { Link } from "react-router-dom";
+import dumpsterFireLogo from "../images/dumpsterfire.png";
+import {
+  Home,
+  Edit,
+  Photo,
+  ListAlt,
+  Message,
+  ShoppingBag,
+  Group,
+  Settings,
+  Build,
+} from "@mui/icons-material";
+import DashboardIcon from "@mui/icons-material/Dashboard";
 
 const createIssueFormSchema = object({
-    title: string().min(3).required(),
-    description: string().min(10).required(),
-    assignee: string().min(3).required(),
-    reporter: string().min(3).required(),
-    priority: mixed().oneOf(['LOW', 'MEDIUM', 'HIGH']).default('LOW'),
-    status: mixed().oneOf(['TO DO', 'IN PROGRESS', 'DONE']).default('TO DO'),
-    due: date().required(),
+  title: string().min(3).required(),
+  description: string().min(10).required(),
+  assignee: string().min(3).required(),
+  reporter: string().min(3).required(),
+  priority: mixed().oneOf(["LOW", "MEDIUM", "HIGH"]).default("LOW"),
+  status: mixed().oneOf(["TO DO", "IN PROGRESS", "DONE"]).default("TO DO"),
+  due: date().required(),
 });
 
 export type CreateIssueFormValues = {
-    title: string;
-    description: string;
-    assignee: string;
-    reporter: string;
-    priority: string;
-    status: string;
-    due: Date;
+  title: string;
+  description: string;
+  assignee: string;
+  reporter: string;
+  priority: string;
+  status: string;
+  due: Date;
 };
 
+const tabs = [
+  { title: "Dashboard", Icon: Home, path: "Dashboard" },
+  { title: "Backing", Icon: Edit, path: "Backing" },
+  { title: "Kanbon Board", Icon: Photo, path: "Kanbon" },
+  { title: "Reports", Icon: ListAlt, path: "Reports" },
+  { title: "Issues Log", Icon: Message, path: "Issues" },
+  { title: "RoadMap", Icon: DashboardIcon, path: "Road" },
+  { title: "Active Sprints", Icon: ShoppingBag, path: "Active" },
+  { title: "Project Settings", Icon: Group, path: "Project" },
+  { title: "User Setting", Icon: Settings, path: "User" },
+  { title: "Tools", Icon: Build, path: "Tools" },
+];
+
 export const Dashboard = () => {
-    const pageHeight = usePageHeight();
-    const navigate = useNavigate();
-    const toast = useToast();
-    const { status, data: signInCheckResult } = useSigninCheck();
+  const pageHeight = usePageHeight();
+  const navigate = useNavigate();
+  const toast = useToast();
+  const { status, data: signInCheckResult } = useSigninCheck();
 
-    const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const createIssueFormInitialValues: CreateIssueFormValues = {
-        title: '',
-        description: '',
-        assignee: '',
-        reporter: '',
-        priority: 'LOW',
-        status: 'TO DO',
-        due: new Date(),
+  const createIssueFormInitialValues: CreateIssueFormValues = {
+    title: "",
+    description: "",
+    assignee: "",
+    reporter: "",
+    priority: "LOW",
+    status: "TO DO",
+    due: new Date(),
+  };
+
+  useEffect(() => {
+    if (status == "success") {
+      if (!signInCheckResult.signedIn) {
+        toast({
+          title: "Cannot Access Dashboard",
+          description: "User is not Authenticated",
+          status: "error",
+          duration: 3500,
+        });
+        navigate("/login");
+      }
+    }
+  }, [signInCheckResult, status]);
+
+  const fetchData = async () => {
+    const res = await axios.get("http://localhost:8000/api/issues");
+    console.log(res.data["queryResult"]);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const submitCreateIssueForm = (
+    values: CreateIssueFormValues,
+    {}: FormikHelpers<CreateIssueFormValues>
+  ) => {
+    const data = {
+      ...values,
+      // Needed to get date into a yy-mm-dd format
+      // See: https://stackoverflow.com/questions/35843050/return-dd-mm-yyyy-from-date-object
+      due: values.due.toISOString().split("T")[0],
     };
 
-    useEffect(() => {
-        if (status == 'success') {
-            if (!signInCheckResult.signedIn) {
-                toast({
-                    title: 'Cannot Access Dashboard',
-                    description: 'User is not Authenticated',
-                    status: 'error',
-                    duration: 3500,
-                });
-                navigate('/login');
-            }
+    axios
+      .post("http://localhost:8000/api/issues/add", data)
+      .then(async (res) => {
+        if (res.status == 200) {
+          toast({
+            title: "Issue Creation Succeeded",
+            description: "Issue was created",
+            status: "success",
+            duration: 3500,
+          });
+          onClose();
+          await fetchData();
         }
-    }, [signInCheckResult, status]);
+      })
+      .catch((_err) => {
+        const err = _err as AxiosError;
 
-    const fetchData = async () => {
-        const res = await axios.get('http://localhost:8000/api/issues');
-        console.log(res.data['queryResult']);
-    };
-    useEffect(() => {
-        fetchData();
-    }, []);
+        toast({
+          title: "Issue Creation Failed",
+          description: err.message,
+          status: "error",
+          duration: 3500,
+        });
+      });
+  };
 
-    const submitCreateIssueForm = (
-        values: CreateIssueFormValues,
-        {}: FormikHelpers<CreateIssueFormValues>
-    ) => {
-        const data = {
-            ...values,
-            // Needed to get date into a yy-mm-dd format
-            // See: https://stackoverflow.com/questions/35843050/return-dd-mm-yyyy-from-date-object
-            due: values.due.toISOString().split('T')[0],
-        };
+  return (
+    <>
+      <div className="dashboard-container">
+        <div className="sidebar">
+          <img
+            className="dashboard-logo"
+            src={dumpsterFireLogo}
+            alt="Dumpster Fire Logo"
+          />
+          {tabs.map(({ title, Icon, path }) => (
+            <Link to={path} className="tab-link">
+              <Icon />
+              <p>{title}</p>
+            </Link>
+          ))}
+        </div>
+        <div className="dashboard-body">
+          <div className="sub-header">
+            <p>Your work</p>
+            <p>Projects</p>
+            <p>Dashboards</p>
+            <p>People</p>
+            <button onClick={onOpen}>Create</button>
+          </div>
+        </div>
+      </div>
 
-        axios
-            .post('http://localhost:8000/api/issues/add', data)
-            .then(async (res) => {
-                if (res.status == 200) {
-                    toast({
-                        title: 'Issue Creation Succeeded',
-                        description: 'Issue was created',
-                        status: 'success',
-                        duration: 3500,
-                    });
-                    onClose();
-                    await fetchData();
-                }
-            })
-            .catch((_err) => {
-                const err = _err as AxiosError;
-
-                toast({
-                    title: 'Issue Creation Failed',
-                    description: err.message,
-                    status: 'error',
-                    duration: 3500,
-                });
-            });
-    };
-
-    return (
-        <>
-            <Box h={pageHeight}>
-                <Center>
-                    <Button onClick={onOpen}>Create</Button>
-                </Center>
-            </Box>
-
-            <Modal
-                isOpen={isOpen}
-                onClose={onClose}
-                size='2xl'
-                blockScrollOnMount={true}
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        size="2xl"
+        blockScrollOnMount={true}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create Issue</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Formik
+              initialValues={createIssueFormInitialValues}
+              validationSchema={createIssueFormSchema}
+              onSubmit={submitCreateIssueForm}
             >
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Create Issue</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <Formik
-                            initialValues={createIssueFormInitialValues}
-                            validationSchema={createIssueFormSchema}
-                            onSubmit={submitCreateIssueForm}
-                        >
-                            {({ errors, touched }) => (
-                                <Form style={{ width: '100%' }}>
-                                    <VStack spacing='4' w='full'>
-                                        <InputFormControl
-                                            isRequired
-                                            label='Issue Title'
-                                            name='title'
-                                            type='text'
-                                            errors={errors.title}
-                                            touched={touched.title}
-                                        />
+              {({ errors, touched }) => (
+                <Form style={{ width: "100%" }}>
+                  <VStack spacing="4" w="full">
+                    <InputFormControl
+                      isRequired
+                      label="Issue Title"
+                      name="title"
+                      type="text"
+                      errors={errors.title}
+                      touched={touched.title}
+                    />
 
-                                        <InputFormControl
-                                            isRequired
-                                            label='Issue Assignee'
-                                            name='assignee'
-                                            type='text'
-                                            errors={errors.assignee}
-                                            touched={touched.assignee}
-                                        />
+                    <InputFormControl
+                      isRequired
+                      label="Issue Assignee"
+                      name="assignee"
+                      type="text"
+                      errors={errors.assignee}
+                      touched={touched.assignee}
+                    />
 
-                                        <InputFormControl
-                                            isRequired
-                                            label='Issue Reporter'
-                                            name='reporter'
-                                            type='text'
-                                            errors={errors.reporter}
-                                            touched={touched.reporter}
-                                        />
+                    <InputFormControl
+                      isRequired
+                      label="Issue Reporter"
+                      name="reporter"
+                      type="text"
+                      errors={errors.reporter}
+                      touched={touched.reporter}
+                    />
 
-                                        <InputFormControl
-                                            isRequired
-                                            label='Issue Status'
-                                            name='status'
-                                            type='text'
-                                            errors={errors.status}
-                                            touched={touched.status}
-                                        />
+                    <InputFormControl
+                      isRequired
+                      label="Issue Status"
+                      name="status"
+                      type="text"
+                      errors={errors.status}
+                      touched={touched.status}
+                    />
 
-                                        <InputFormControl
-                                            isRequired
-                                            label='Priority'
-                                            name='priority'
-                                            type='text'
-                                            errors={errors.priority}
-                                            touched={touched.priority}
-                                        />
+                    <InputFormControl
+                      isRequired
+                      label="Priority"
+                      name="priority"
+                      type="text"
+                      errors={errors.priority}
+                      touched={touched.priority}
+                    />
 
-                                        <DatePickerFormControl
-                                            name='due'
-                                            label='Date Due'
-                                        />
+                    <DatePickerFormControl name="due" label="Date Due" />
 
-                                        <InputFormControl
-                                            isRequired
-                                            label='Description'
-                                            name='description'
-                                            errors={errors.description}
-                                            touched={touched.description}
-                                            h='185px'
-                                            textArea={{ resize: 'none' }}
-                                        />
-                                    </VStack>
-                                    <HStack
-                                        w='full'
-                                        mt='4'
-                                        justifyContent='flex-end'
-                                    >
-                                        <Button onClick={onClose}>Close</Button>
-                                        <Button
-                                            type='submit'
-                                            variant='ghost'
-                                            colorScheme='purple'
-                                        >
-                                            Create
-                                        </Button>
-                                    </HStack>
-                                </Form>
-                            )}
-                        </Formik>
-                    </ModalBody>
-                </ModalContent>
-            </Modal>
-        </>
-    );
+                    <InputFormControl
+                      isRequired
+                      label="Description"
+                      name="description"
+                      errors={errors.description}
+                      touched={touched.description}
+                      h="185px"
+                      textArea={{ resize: "none" }}
+                    />
+                  </VStack>
+                  <HStack w="full" mt="4" justifyContent="flex-end">
+                    <Button onClick={onClose}>Close</Button>
+                    <Button type="submit" variant="ghost" colorScheme="purple">
+                      Create
+                    </Button>
+                  </HStack>
+                </Form>
+              )}
+            </Formik>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
